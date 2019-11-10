8385e4e678c95e83c16c143f923832f934a3a6dc
pedropefj <pedro.pefj@hotmail.com>
2019-11-10 19:43:35 -0200

    Criando security group e vinculando as instancias criadas

--- main.tf
+++ main.tf
@@ -8,7 +8,28 @@ resource "aws_instance" "dev" {
   instance_type = "t2.micro"
   key_name = "terraform-aws"
   tags = {
-      Name = "dev - ${count.index}"
+    Name = "dev${count.index}"
+  }
+
+  vpc_security_group_ids = ["sg-0b85f82c2147e0b67"] //aws ec2 describe-security-groups
+}
+
+resource "aws_security_group" "acesso-ssh" {
+  name        = "acesso-ssh"
+  description = "Acesso SSH"
+
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [
+        "201.17.210.184/32"
+    ]
+  }
+
+  tags = {
+    Name = "ssh"
   }
 }
 
